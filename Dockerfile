d9ae0011f06a2a50120f01546c6c797e91ec8477
sarut yentakham <sarut.y@bitkub.com>
2018-05-29 14:50:08 +0700

    Prototype Docker + Laravel

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+RUN sh start.sh install
---
+++ laravel/app/Http/Controllers/Test/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use DB;
+
+class IndexController extends Controller
+{
+    public function index() {
+        echo 1;exit;
+    }
+}
